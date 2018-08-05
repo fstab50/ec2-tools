de6a4564aaf32aa4bb4a8279551cd8dca8e9c9f2
blakeca00 <blakeca00@gmail.com>
2018-08-05 18:34:34 -0500

    ec2tools/help_menu.py: corrections, updates

--- ec2tools/help_menu.py
+++ ec2tools/help_menu.py
@@ -40,6 +40,7 @@ menu_body = Colors.BOLD + Colors.WHITE + """
                            [-d, --details  ]
                            [-f, --format   <value> ]
                            [-n, --filename <value> ]
+                           [-r, --region <value> ]
                            [-d, --debug    ]
                            [-h, --help     ]
                            [-V, --version  ]
@@ -49,16 +50,16 @@ menu_body = Colors.BOLD + Colors.WHITE + """
         -i, --image""" + Colors.RESET + """ (string) : Amazon Machine Image Operating System type.
             Must be value from the following list.
 
-                Valid Values, EC2 Amazon Machine Image (AMI) OS_TYPE:
+                EC2 Amazon Machine Image (AMI) OS_TYPE:
 
                     - """ + AMI + """amazonlinux1""" + RESET + """    :   Amazon Linux v1 (2018)
                     - """ + AMI + """amazonlinux2""" + RESET + """    :   Amazon Linux v2 (2017.12+)
                     - """ + AMI + """centos5""" + RESET + """         :   CentOS 5 (RHEL 5+)
                     - """ + AMI + """centos6""" + RESET + """         :   CentOS 6 (RHEL 6+)
                     - """ + AMI + """centos7""" + RESET + """         :   CentOS 7 (RHEL 7+)
-                    - """ + AMI + """redhat7.5""" + RESET + """       :   Redhat Enterprise Linux 7.5
+                    - """ + AMI + """redhat7.3""" + RESET + """       :   Redhat Enterprise Linux 7.3
                     - """ + AMI + """redhat7.4""" + RESET + """       :   Redhat Enterprise Linux 7.4
-                    - """ + AMI + """redhat7.4""" + RESET + """       :   Redhat Enterprise Linux 7.3
+                    - """ + AMI + """redhat7.5""" + RESET + """       :   Redhat Enterprise Linux 7.5
                     - """ + AMI + """ubuntu14.04""" + RESET + """     :   Ubuntu Linux 14.04
                     - """ + AMI + """ubuntu16.04""" + RESET + """     :   Ubuntu Linux 16.04
                     - """ + AMI + """ubuntu18.04""" + RESET + """     :   Ubuntu Linux 18.04
@@ -74,6 +75,10 @@ menu_body = Colors.BOLD + Colors.WHITE + """
     """ + Colors.BOLD + Colors.WHITE + """
         -n, --filename""" + Colors.RESET + """ <value>: Write output to a filesystem object with a name
             specified in the --filename parameter
+    """ + Colors.BOLD + Colors.WHITE + """
+        -r, --region""" + Colors.RESET + """ <value>: Amazon Web Services Region Code. When provided as
+            a parameter, """ + PACKAGE + """ returns the AMI image only for a particular
+            AWS region.  Examples: ap-northeast-1 (Tokyo, Japan)
     """ + Colors.BOLD + Colors.WHITE + """
         -d, --debug""" + Colors.RESET + """ :  Turn on verbose log output
     """ + Colors.BOLD + Colors.WHITE + """
