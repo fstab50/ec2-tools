4e6c8e0b6dba0c686ccaeed93ad798de4e4db945
Blake Huber <blake.huber@atos.net>
2018-08-10 07:06:14 -0500

    hooks/post-commit-versionupdate.py added

---
+++ hooks/post-commit-versionupdate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+
+   - Summary:   Post commit hook, updates version throughout project
+   - Location:  .git/hooks
+   - Filename:  commit-msg
+
+"""
+import os
+import sys
+
+PACKAGE = 'ec2tools'
+
+sys.path.insert(0, os.path.abspath(PACKAGE))
+from PACKAGE._version import __version__
+sys.path.pop(0)
+
+
+with open('README.md', 'a') as f1:
+    f2 = f1.readlines()
+    for line in f2:
+        if 'Version:' in line:
+            prepend = line.split(' ')[0]
+            line = prepend + ', Version: ' + __version__
+            f2.write(line)
+            
+sys.exit(0)
