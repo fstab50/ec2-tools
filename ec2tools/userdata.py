288428fa4ce474ba4a8d225fbfa7ed2967a70f39
Blake Huber <blakeca00@gmail.com>
2019-01-01 10:54:04 -0600

    userdata.py: converted to encapsulated bash script

--- ec2tools/userdata.py
+++ ec2tools/userdata.py
@@ -1,6 +1,21 @@
+"""
+Summary.
+
+    Bash userdata script for EC2 initialization encapsulated
+    as a python module
+
+"""
+
+content = '''
+
 #!/usr/bin/env bash
 
-function os_identify(){
+PYTHON2_SCRIPT='python2-userdata.py'
+PYTHON3_SCRIPT='python3-userdata.py'
+SOURCE_URL='https://s3.us-east-2.amazonaws.com/awscloud.center/files'
+
+
+function os_type(){
     local bin
     if [[ $(which rpm) ]]; then
         echo "redhat"
@@ -9,9 +24,50 @@ function os_identify(){
     fi
 }
 
+function download(){
+    local fname="$1"
+    if [[ $(which wget) ]]; then
+        wget "$SOURCE_URL/$fname"
+    else
+        curl -o $fname  "$SOURCE_URL/$fname"
+    fi
+    if [[ -f $fname ]]; then
+        logger "$fname downloaded successfully"
+        return 0
+    else
+        logger "ERROR:  Problem downloading $fname"
+        return 1
+    fi
+}
+
+
+# log os type
 if [[ $(which logger) ]]; then
-    logger "Package manager type: $(os_identify)"
+    logger "Package manager type: $(os_type)"
 else
-    echo "Package manager type: $(os_identify)" > /root/userdata.msg
+    echo "Package manager type: $(os_type)" > /root/userdata.msg
 fi
+
+
+# install wget if available
+if [[ "$(os_type)" = "redhat" ]]; then
+    yum install -y wget
+else
+    apt install -y wget
+fi
+
+
+# download and execute python userdata script
+if [[ $(which python3) ]]; then
+
+    if download "$PYTHON3_SCRIPT"; then
+        python3 "$PYTHON3_SCRIPT"
+    fi
+
+elif download "$PYTHON2_SCRIPT"; then
+    python "$PYTHON2_SCRIPT"
+fi
+
 exit 0
+
+'''
