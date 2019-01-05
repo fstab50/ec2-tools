ebbff2168345238d66bcf80d118966b1f5d7f9ce
Blake Huber <blakeca00@gmail.com>
2019-01-05 11:56:24 -0600

    scripts/s3upload.sh

--- scripts/s3upload.sh
+++ scripts/s3upload.sh
@@ -13,27 +13,48 @@ profilename='gcreds-da-atos'
 acltype='public-read'
 git_root=$(git rev-parse --show-toplevel 2>/dev/null)
 pwd=$PWD
+config_dir="$HOME/.config/bash"
+errors='/dev/null'
+debug="$1"
+
+if [[ -f $config_dir/colors.sh ]]; then
+    source "$config_dir/colors.sh"
+fi
+
+# colors
+accent=$pv_orange
+bd=$(echo -e $bold)
+rst=$(echo -e $resetansi)
 
 declare -a artifacts
-#artifacts=(
-#    'python2_generic.py'
-#    'python3_generic.py'
-#)
+artifacts=(
+    'python2_generic.py'
+    'python3_generic.py'
+    'userdata.sh'
+)
 
 cd "$git_root/userdata" || echo "ERROR: unable to cd to userdata directory"
 
 artifacts=(  "$(ls .)" )
-echo "${artifacts[@]}"
+
+if [[ $debug ]]; then
+    echo -e "\tArtifacts for upload are:"
+    for f in "${artifacts[@]}"; do
+        echo -e "\t\t$f"
+    done
+fi
 
 
 for f in "${artifacts[@]}"; do
-    echo "$pkg:  uploading artifact $f to Amazon S3..."
-    aws s3 cp $f s3://$bucketname/files/$f --profile $profilename
+    echo -e "\n\t[${bd}$pkg${rst}]:  uploading artifact ${accent}$f${rst} to Amazon S3...\n"
+    r=$(aws s3 cp $f s3://$bucketname/files/$f --profile $profilename 2>$errors)
+    if [[ $debug ]]; then echo -e "\t$r"; fi
 done
 
 for f in "${artifacts[@]}"; do
-    echo "$pkg:  setting acl on artifact $f..."
-    aws s3api put-object-acl --key files/$f --acl $acltype --bucket $bucketname --profile $profilename
+    echo -e "\n\t[${bd}$pkg${rst}]: setting acl on artifact ${accent}$f${rst}...\n"
+    r=$(aws s3api put-object-acl --key files/$f --acl $acltype --bucket $bucketname --profile $profilename 2>$errors)
+    if [[ $debug ]]; then echo -e "\t$r"; fi
 done
 
 cd $pwd || exit 1
