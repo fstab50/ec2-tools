582d9f392a62c708c12f08ecc276ad3f1186f9b4
Blake Huber <blakeca00@gmail.com>
2019-01-05 05:43:17 -0600

    setup.py: add postinstall script, data_files for placing userdata scripts during install

--- setup.py
+++ setup.py
@@ -35,9 +35,118 @@ requires = [
 ]
 
 
+_package = 'ec2tools'
+_root = os.path.abspath(os.path.dirname(__file__))
+_comp_fname = 'ec2tools-completion.bash'
+
+
+def _root_user():
+    """
+    Checks localhost root or sudo access
+    """
+    if os.geteuid() == 0:
+        return True
+    elif subprocess.getoutput('echo $EUID') == '0':
+        return True
+    return False
+
+
+def get_fullpath(relpath):
+    return os.path.join(_root, 'data', relpath)
+
+
+def create_artifact(object_path, type):
+    """Creates post install filesystem artifacts"""
+    if type == 'file':
+        with open(object_path, 'w') as f1:
+            f1.write(sourcefile_content())
+    elif type == 'dir':
+        os.makedirs(object_path)
+
+
+def os_parityPath(path):
+    """
+    Converts unix paths to correct windows equivalents.
+    Unix native paths remain unchanged (no effect)
+    """
+    path = os.path.normpath(os.path.expanduser(path))
+    if path.startswith('\\'):
+        return 'C:' + path
+    return path
+
+
+class PostInstallDevelop(develop):
+    """ post-install, development """
+    def run(self):
+        subprocess.check_call("bash scripts/post-install-dev.sh".split())
+        develop.run(self)
+
+
+class PostInstall(install):
+    """
+    Summary.
+
+        Postinstall script to place prompt completion artifacts
+        on local filesystem
+
+    """
+    def run(self):
+        config_dir = user_home() + '/.config/' + _package
+        userdata_dir = user_home() + '/.config/' + _package + '/userdata'
+        lconfig_dir = user_home() + '/.config/' + _package + '/launchconfigs'
+
+        if not os.path.exists(os_parityPath(config_dir)):
+            create_artifact(os_parityPath(config_dir), 'dir')
+        if not os.path.exists(os_parityPath(userdata_dir)):
+            create_artifact(os_parityPath(userdata_dir), 'dir')
+        if not os.path.exists(os_parityPath(lconfig_dir)):
+            create_artifact(os_parityPath(lconfig_dir), 'dir')
+        install.run(self)
+
+
+def preclean(dst):
+    if os.path.exists(dst):
+        os.remove(dst)
+    return True
+
+
 def read(fname):
     basedir = os.path.dirname(sys.argv[0])
     return open(os.path.join(basedir, fname)).read()
+
+
+def sourcefile_content():
+    sourcefile = """
+    for bcfile in ~/.bash_completion.d/* ; do
+        [ -f "$bcfile" ] && . $bcfile
+    done\n
+    """
+    return sourcefile
+
+
+def user_home():
+    """
+    Summary.
+
+        os specific home dir for current user
+
+    Returns:
+        home directory for user (str)
+        If sudo used, still returns user home (Linux only)
+    """
+    try:
+        if platform.system() == 'Linux':
+            return os.getenv('HOME')
+
+        elif platform.system() == 'Windows':
+            username = os.getenv('username')
+            return 'C:\\Users\\' + username
+
+        elif platform.system() == 'Java':
+            print(f'Unsupported of {os_type}')
+            return ''
+    except OSError as e:
+        raise e
 
 
 setup(
@@ -63,6 +172,15 @@ setup(
     packages=find_packages(exclude=['docs', 'scripts', 'assets']),
     install_requires=requires,
     python_requires='>=3.6, <4',
+    cmdclass={
+        'install': PostInstall
+    },
+    data_files=[
+        (
+            user_home() + '/' + '.config/' + _package + '/userdata',
+            ['userdata/python2_generic.py', 'userdata/python3_generic.py']
+        )
+    ],
     entry_points={
         'console_scripts': [
             'machineimage=ec2tools.current_ami:init_cli',
