a78ed9b75a52c13841817394e9b2dec0532f311b
Blake Huber <blakeca00@gmail.com>
2019-01-27 07:53:12 -0600

    bash/ec2tools-completion.bash: comment updates

--- bash/ec2tools-completion.bash
+++ bash/ec2tools-completion.bash
@@ -203,10 +203,13 @@ function _runmachine_completions(){
     ##
     ##  Completion structures for runmachine exectuable
     ##
-    local numargs subcommands
-    local cur
-    local prev
-    local initcmd
+    local commands                  #  commandline parameters (--*)
+    local subcommands               #  subcommands are parameters provided after a command
+    local image_subcommands         #  parameters provided after --image command
+    local numargs                   #  integer count of number of commands, subcommands
+    local cur                       #  completion word at current index position 0 in COMP_WORDS array
+    local prev                      #  completion word at current index position -1 in COMP_WORDS array
+    local initcmd                   #  completion word at current index position -2 in COMP_WORDS array
 
     config_dir="$HOME/.config/ec2tools"
     cur="${COMP_WORDS[COMP_CWORD]}"
