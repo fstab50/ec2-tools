9e4b13d4fd529a4864ff81932aefe4047ee21cc9
Blake Huber <blakeca00@gmail.com>
2019-08-11 10:21:54 -0500

    machineimage help menu updates

--- ec2tools/help_menu.py
+++ ec2tools/help_menu.py
@@ -6,7 +6,7 @@ Help Menu
 
 """
 
-from variables import c, blg, rst
+from ec2tools.variables import c, bgr, rst
 
 PACKAGE = 'machineimage'
 PKG_ACCENT = c.ORANGE
@@ -15,8 +15,8 @@ AMI = c.DARK_CYAN
 BD = c.BOLD
 
 synopsis_cmd = (
-    rst + PKG_ACCENT + c.BOLD + PACKAGE + RESET +
-    PARAM_ACCENT + '  --image ' + rst + '{' + AMI + 'OS_TYPE' + RESET + '}' +
+    rst + PKG_ACCENT + c.BOLD + PACKAGE + rst +
+    PARAM_ACCENT + '  --image ' + rst + '{' + AMI + 'OS_TYPE' + rst + '}' +
     PARAM_ACCENT + '  --profile' + rst + ' <value>' +
     PARAM_ACCENT + '  --region' + rst + ' <value>'
     )
@@ -48,20 +48,20 @@ menu_body = c.BOLD + c.WHITE + """
         -i, --image""" + rst + """  (string):  Amazon  Machine  Image Operating System type
             Returns the latest AMI of the type specified from the list below
 
-                      """ + blg + """Amazon EC2 Machine Images (AMI)""" + rst + """:
+                      """ + bgr + """Amazon EC2 Machine Images (AMI)""" + rst + """:
 
-                  - """ + AMI + """amazonlinux1""" + RESET + """  :  Amazon Linux v1 (2018)
-                  - """ + AMI + """amazonlinux2""" + RESET + """  :  Amazon Linux v2 (2017.12+)
-                  - """ + AMI + """centos6""" + RESET + """       :  CentOS 6 (RHEL 6+)
-                  - """ + AMI + """centos7""" + RESET + """       :  CentOS 7 (RHEL 7+)
-                  - """ + AMI + """redhat""" + RESET + """        :  Latest Redhat Enterprise Linux
-                  - """ + AMI + """redhat7.4""" + RESET + """     :  Redhat Enterprise Linux 7.4
-                  - """ + AMI + """redhat7.5""" + RESET + """     :  Redhat Enterprise Linux 7.5
-                  - """ + AMI + """ubuntu14.04""" + RESET + """   :  Ubuntu Linux 14.04
-                  - """ + AMI + """ubuntu16.04""" + RESET + """   :  Ubuntu Linux 16.04
-                  - """ + AMI + """ubuntu18.04""" + RESET + """   :  Ubuntu Linux 18.04
-                  - """ + AMI + """windows2012""" + RESET + """   :  Microsoft Windows Server 2012 R2
-                  - """ + AMI + """windows2016""" + RESET + """   :  Microsoft Windows Server 2016
+                  - """ + AMI + """amazonlinux1""" + rst + """  :  Amazon Linux v1 (2018)
+                  - """ + AMI + """amazonlinux2""" + rst + """  :  Amazon Linux v2 (2017.12+)
+                  - """ + AMI + """centos6""" + rst + """       :  CentOS 6 (RHEL 6+)
+                  - """ + AMI + """centos7""" + rst + """       :  CentOS 7 (RHEL 7+)
+                  - """ + AMI + """redhat""" + rst + """        :  Latest Redhat Enterprise Linux
+                  - """ + AMI + """redhat7.4""" + rst + """     :  Redhat Enterprise Linux 7.4
+                  - """ + AMI + """redhat7.5""" + rst + """     :  Redhat Enterprise Linux 7.5
+                  - """ + AMI + """ubuntu14.04""" + rst + """   :  Ubuntu Linux 14.04
+                  - """ + AMI + """ubuntu16.04""" + rst + """   :  Ubuntu Linux 16.04
+                  - """ + AMI + """ubuntu18.04""" + rst + """   :  Ubuntu Linux 18.04
+                  - """ + AMI + """windows2012""" + rst + """   :  Microsoft Windows Server 2012 R2
+                  - """ + AMI + """windows2016""" + rst + """   :  Microsoft Windows Server 2016
 
     """ + c.BOLD + c.WHITE + """
         -p, --profile""" + rst + """  (string):  Profile name of an IAM user present in the
@@ -81,8 +81,8 @@ menu_body = c.BOLD + c.WHITE + """
             as parameter, """ + PACKAGE + """ returns the Amazon Machine image only
             for a particular AWS region.  Region code examples:
 
-                        - """ + BD + """ap-northeast-1""" + RESET + """  (Tokyo, Japan)
-                        - """ + BD + """eu-central-1""" + RESET + """    (Frankfurt, Germany)
+                        - """ + bgr + """ap-northeast-1""" + rst + """  (Tokyo, Japan)
+                        - """ + bgr + """eu-central-1""" + rst + """    (Frankfurt, Germany)
 
             If the region parameter is omitted,  """ + PACKAGE + """ returns Amazon
             Machine Images for all regions.
