538c2605ff579c72b6d3d5ed40c8b8e92ec3dc8a
Blake Huber <blakeca00@gmail.com>
2019-10-01 07:02:33 -0500

    setup.py: post testing, incrment dep versions to eliminate installation issues

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ import ec2tools
 
 
 requires = [
-    'boto3>=1.7.1',
-    'botocore>=1.10.1',
-    'pyaws>=0.2.33',
+    'boto3>=1.9.1',
+    'colorama==0.3.9',
+    'pyaws>=0.4.1',
     'Pygments>=2.4.0',
     'requests',
     'VeryPrettyTable'
